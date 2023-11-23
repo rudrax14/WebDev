f0c000f7a4410715a016bc64eb63a4d89fff3390
rudrax14 <m.rudra2003@gmail.com>
2023-11-24 00:22:48 +0530

    DAS

--- BackEnd/Auth App/middleware/auth.js
+++ BackEnd/Auth App/middleware/auth.js
@@ -5,18 +5,18 @@ require("dotenv").config();
 exports.auth = (req, res, next) => {
     try {
 
-        // console.log("Body", req.body.token);
-        // console.log("Cookies", req.cookies.token);
-        // console.log("Header", req.header("Authorization").replace("Bearer", " "));
+        console.log("Body", req.body.token);
+        console.log("Cookies", req.cookies.token);
+        console.log("Header", req.header("Authorization").replace("Bearer", " "));
 
         // const token = req.body.token;
         // const token = req.cookie.token 
         const token = req.body.token || req.cookies.token || req.header("Authorization").replace("Bearer ", "");
 
-        if(!token || token === undefined) {
+        if (!token || token === undefined) {
             return res.status(401).json({
-                success:false,
-                message:'Token Missing',
+                success: false,
+                message: 'Token Missing',
             });
         }
         // verify the token 
